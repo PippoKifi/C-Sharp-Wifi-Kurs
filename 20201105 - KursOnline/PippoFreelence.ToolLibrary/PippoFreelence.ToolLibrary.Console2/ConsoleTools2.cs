4ca3dbcc44faa8d7633028ddfc3364767d06a7cc
Pippo Kifi <kipippo@posteo.at>
2020-11-06 17:17:20 +0100

    public class ConsoleTools2 erstellt

---
+++ 20201105 - KursOnline/PippoFreelence.ToolLibrary/PippoFreelence.ToolLibrary.Console2/ConsoleTools2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PippoFreelence.ToolLibrary.ConsoleIO2
+{
+    public class ConsoleTools2
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="InputString"></param>
+        /// <returns></returns>
+        public static int StringCounter(string InputString)
+        { 
+        
+        
+        }
+
+        
+        //Console Input Analyser (was für eine eingabe hat der User gemacht)
+
+
+
+    }
+}
