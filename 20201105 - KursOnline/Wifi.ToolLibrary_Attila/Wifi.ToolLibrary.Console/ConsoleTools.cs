11df7bd2f157433bac132a51ecdeaf5a7ad231ed
Pippo Kifi <kipippo@posteo.de>
2020-11-10 16:47:47 +0100

    Projekt Sync from Attila

---
+++ 20201105 - KursOnline/Wifi.ToolLibrary_Attila/Wifi.ToolLibrary.Console/ConsoleTools.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wifi.ToolLibrary.ConsoleIo
+{
+    public class ConsoleTools
+    {
+        /// <summary>
+        /// Generates colored messages for the console output
+        /// </summary>
+        /// <param name="message">The message to display</param>
+        /// <param name="messageColor">The color of the message</param>
+        public static void DisplayColoredMessage(string message, ConsoleColor messageColor)
+        {
+            ConsoleColor oldColor = Console.ForegroundColor;
+            Console.ForegroundColor = messageColor;
+
+            Console.WriteLine(message);
+
+            Console.ForegroundColor = oldColor;
+        }
+
+        /// <summary>
+        /// Generates colored messages for the console output
+        /// </summary>
+        /// <param name="message">The message to display</param>
+        /// <param name="messageColor">The color of the message</param>
+        public static void DisplayColoredMessage(string message)
+        {
+            DisplayColoredMessage(message, ConsoleColor.Yellow);
+        }
+
+
+
+        /// <summary>
+        /// Reads an integer value from console input.
+        /// </summary>
+        /// <param name="inputPrompt">Prompt for the user</param>
+        /// <returns>Input value as integer</returns>
+        public static int GetInt(string inputPrompt)
+        {
+            int userInputValue = 0;
+            bool userInputIsValid = false;
+
+            do
+            {
+                Console.Write(inputPrompt);
+                try
+                {
+                    //read input value and convert to int
+                    userInputValue = int.Parse(Console.ReadLine());
+                    userInputIsValid = true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                    userInputIsValid = false;
+                }
+            }
+            while (!userInputIsValid);
+
+            return userInputValue;
+        }
+
+        /// <summary>
+        /// Reads an double value from console input.
+        /// </summary>
+        /// <param name="inputPrompt">Prompt for the user</param>
+        /// <returns></returns>
+        public static double GetDouble(string inputPrompt)
+        {
+            double userInputValue = 0;
+            bool userInputIsValid = false;
+
+            do
+            {
+                Console.Write(inputPrompt);
+                try
+                {
+                    //read input value and convert to double
+                    userInputValue = double.Parse(Console.ReadLine());
+                    userInputIsValid = true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                    userInputIsValid = false;
+                }
+            }
+            while (!userInputIsValid);
+
+            return userInputValue;
+        }
+
+        /// <summary>
+        /// Reads an DateTime value from console input. The input format must be [dd.MM.yyyy hh:mm:ss].
+        /// </summary>
+        /// <param name="inputPrompt">Prompt for the user</param>
+        /// <returns></returns>
+        public static DateTime GetDateTime(string inputPrompt)
+        {
+            DateTime userInputValue = DateTime.MinValue;
+            bool userInputIsValid = false;
+
+            string inputFormat = "dd.MM.yyyy hh:mm:ss";
+
+            do
+            {
+                Console.Write(inputPrompt);
+                try
+                {
+                    //read input value and convert to DateTime
+                    userInputValue = DateTime.ParseExact(Console.ReadLine(), inputFormat, CultureInfo.InvariantCulture);
+                    userInputIsValid = true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                    userInputIsValid = false;
+                }
+            }
+            while (!userInputIsValid);
+
+            return userInputValue;
+        }
+
+        /// <summary>
+        /// Reads an string value from console input.
+        /// </summary>
+        /// <param name="inputPrompt">Prompt for the user</param>
+        /// <returns></returns>
+        public static string GetString(string inputPrompt)
+        {
+            Console.Write(inputPrompt);
+
+            //read input value and convert to string
+            return Console.ReadLine();
+        }
+
+
+        public static void HelloWorld()
+        {
+            Console.WriteLine("Hi!");
+        }
+    }
+}
---
+++ 20201109 - KursOnline/TeilnehmerVerwaltung_v2/TeilnehmerVerwaltung_v2/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.IO;
+
+using Wifi.ToolLibrary.ConsoleIo;
+
+namespace TeilnehmerVerwaltung_v2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+             * Schreiben Sie eine einfache Applikation mit der Teilnehmer-Daten verwaltet 
+             * werden können.
+             * Teilnehmerdaten sollen:
+             * 
+             *  - Eingabe
+             *  - tabellarische Ausgabe
+             *  - Ausgabe in eine Text-Datei (wahlweise)
+             * 
+             * Welche Teilnehmerdaten:
+             * 
+             *   - TeilnehmerID (= Guid)
+             *   - Name & Nachname
+             *   - Strasse, HausNr, Plz, Ort
+             *   - Geburtsdatum
+             *   
+             */
+
+            int teilnehmerCount = 0;
+            Teilnehmer[] meineTeilnehmer;
+            
+
+            //Anzahl einlesen
+            teilnehmerCount = ConsoleTools.GetInt("Anzahl der Teilnehmer eingeben: ");
+
+            //Teilnehmerdaten einlesen
+            meineTeilnehmer = ReadTeilnehmerData(teilnehmerCount);
+
+            //Teilnehmerdaten ausgeben
+            DisplayTeilnehmerData(meineTeilnehmer);
+
+            //Teilnehmerdaten optional als Datei sichern
+            SaveDataToFile(meineTeilnehmer);            
+        }
+
+        static void SaveDataToFile(Teilnehmer[] meineTeilnehmer)
+        {
+            string userInput = ConsoleTools.GetString("Wollen Sie die Daten in einer Datei sichern (j/n)?");
+            if (userInput.ToLower() == "j")
+            {
+                string fileName = ConsoleTools.GetString("Dateiname: ");
+                if (!string.IsNullOrWhiteSpace(fileName))
+                {
+                    try
+                    {
+                        SaveData(fileName, meineTeilnehmer);
+                        Console.WriteLine($"Daten wurden in die Datei {fileName} geschrieben.");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Daten konnten nicht gesichert werden.");
+                        Console.WriteLine($"ERROR: {ex.Message}");
+                    }
+                }
+            }            
+        }
+
+        static void SaveData(string fileName, Teilnehmer[] meineTeilnehmer)
+        {
+            using (StreamWriter sw = new StreamWriter(fileName, true))
+            {
+                foreach (Teilnehmer t in meineTeilnehmer)
+                {                    
+                    sw.WriteLine($"{t.Vorname}, {t.Nachname}, {t.Strasse}, {t.HausNr}, {t.Plz}, {t.Ort}, {t.Geburtsdatum}");
+                }
+            }
+        }
+
+        static void DisplayTeilnehmerData(Teilnehmer[] meineTeilnehmer)
+        {
+            foreach (Teilnehmer t in meineTeilnehmer)
+            {
+                Console.WriteLine($"{t.Vorname} {t.Nachname} [{t.Geburtsdatum.Year}] - {t.Plz} {t.Ort}");
+            }
+        }
+
+        static Teilnehmer[] ReadTeilnehmerData(int teilnehmerCount)
+        {
+            Teilnehmer[] myTempDataList = new Teilnehmer[teilnehmerCount];
+
+            for (int i = 0; i < teilnehmerCount; i++)
+            {
+                Console.WriteLine($"\nBitte Daten für Teilnehmer {i+1} eingeben:");
+
+                myTempDataList[i].Vorname = ConsoleTools.GetString("\tVorname: ");
+                myTempDataList[i].Nachname = ConsoleTools.GetString("\tNachname: ");
+
+                myTempDataList[i].Strasse = ConsoleTools.GetString("\tStrasse: ");
+                myTempDataList[i].HausNr = ConsoleTools.GetString("\tHausNr: ");
+                myTempDataList[i].Ort = ConsoleTools.GetString("\tOrt: ");
+                myTempDataList[i].Plz = ConsoleTools.GetInt("\tPlz: ");
+
+                myTempDataList[i].Geburtsdatum = ConsoleTools.GetDateTime("\tGeburtsdatum: ");
+            }
+
+            return myTempDataList;
+        }
+    }
+}
