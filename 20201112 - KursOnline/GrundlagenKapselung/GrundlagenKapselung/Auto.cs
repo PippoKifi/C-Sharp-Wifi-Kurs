8534dd4a816007ee61a4a5aa7f20eef094c484cb
Pippo Kifi <kipippo@posteo.de>
2020-11-12 21:23:20 +0100

    GrundlagenKapselung

---
+++ 20201112 - KursOnline/GrundlagenKapselung/GrundlagenKapselung/Auto.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Wifi.ToolLibrary.ConsoleIo;
+
+namespace GrundlagenKapselung
+{
+    public class Auto
+    {
+        //EIGENSCHAFTEN (Feldern -> Niemals Öffentlich!)
+        private string _type;
+        private string _description;
+        private int _maxSpeed;
+        private int _currentSpeed;
+
+        //##### KONSTRUKTOR
+        public Auto()
+        {
+            _type = "Opel Astra";
+            _description = "Ein std. Fahrzeug für die Familie";
+            _maxSpeed = 175;
+            _currentSpeed = 0;
+        }
+        public Auto(string type, string description, int maxSpeed)
+        {
+            _type = type;
+            _description = description;
+            _maxSpeed = maxSpeed;
+            _currentSpeed = 0;
+        }
+        public Auto(string type, string description)
+        {
+            _type = type;
+            _description = description;
+
+            _maxSpeed = 175;
+            _currentSpeed = 0;
+        }
+
+        //##### EIEGNSCHAFTEN Zugriffs & änderungs-Methoden (Get \ Set)
+        //Zustand informationen kapseln und nach aussen kommunizieren (Privat macht keine sinn!)
+        //Set-Block weglassen -> Schreibgeschützt!
+        //Im hintergrund erstellt VS Methoden 
+        public string Type
+        {
+            get
+            {
+                return _type;
+            }
+
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    _type = value;
+                }
+            }
+        }
+        public string Description
+        {
+            get
+            {
+                return _description;
+            }
+        }
+        public int MaxSpeed
+        {
+            get
+            {
+                return _maxSpeed;
+            }
+        }
+        public int CurrentSpeed
+        {
+            get
+            {
+                return _currentSpeed;
+            }
+        }
+
+        public void SpeedUp(int delta)
+        {
+            if (_currentSpeed + delta > _maxSpeed || _currentSpeed + delta < 0)
+            {
+                ConsoleTools.DisplayColoredMessage($"ERROR: Die aktuelle Geschwindigkeit kann nicht um {delta} verändert werden.", ConsoleColor.Red);
+                return;
+            }
+
+            //update current speed
+            _currentSpeed += delta;
+        }
+
+        public void DisplayInfos()
+        {
+            ConsoleTools.DisplayColoredMessage($"{_type} - {_description}");
+            ConsoleTools.DisplayColoredMessage($"Max. Speed: {_maxSpeed} km/h Current Speed: {_currentSpeed} km/h");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 20201112 - KursOnline/GrundlagenKapselung/GrundlagenKapselung/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GrundlagenKapselung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Auto car = new Auto();
+            car.DisplayInfos();
+
+            for (int i = 0; i < 5; i++)
+            {
+                car.SpeedUp(+10);
+                car.DisplayInfos();
+            }
+
+            Console.WriteLine($"Status of {car.Type} |  ");
+
+            while (car.CurrentSpeed < 100)
+            {
+                car.SpeedUp(10);
+                Console.WriteLine($"Status of {car.Type} |  ");
+                Thread.Sleep(1000);
+            }
+
+        }
+    }
+}
