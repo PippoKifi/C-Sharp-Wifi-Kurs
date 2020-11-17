874fc0530a2fcab0a3ee56fd3def015609bbefba
user <user@WIN10DEV>
2020-11-17 21:27:53 +0100

    GrundlagenVererbung

---
+++ 20201117 - KursDO/GrundlagenVererbung/GrundlagenVererbung/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrundlagenVererbung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sales susi = new Sales("Max","Mustermann", new DateTime(2000,5,1), 1500.90m, 0.015);
+            Console.WriteLine(susi.GetInfoString());
+        }
+    }
+}
---
+++ 20201117 - KursDO/GrundlagenVererbung/GrundlagenVererbung/Sales.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrundlagenVererbung
+{
+    class Sales : Emloyee //Ableitung - Die Klasse 'Sales' erbt jetzt die Klasse 'Emloyee'
+    {
+        private Double _provision;
+
+        //Konstruktor | Instanzieren
+        public Sales(string vorname, string nachname, DateTime geburtsDatum, decimal gehalt,double _provision) 
+            : base(vorname, nachname, geburtsDatum, gehalt)
+        {
+            _provision = Provision;
+        }
+
+
+        public Double Provision
+        {
+            get { return _provision; }
+            set { _provision = value; }
+        }
+
+    }
+}
