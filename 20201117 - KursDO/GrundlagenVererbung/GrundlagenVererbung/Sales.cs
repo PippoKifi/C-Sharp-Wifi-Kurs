fbaf306fed04a8c8fcf0aed92170c4c3a4fdf03b
PippoKifi@Home <kipippo@posteo.at>
2020-11-19 21:47:49 +0100

    Kleine Otische Code verbesserung

--- 20201117 - KursDO/GrundlagenVererbung/GrundlagenVererbung/Sales.cs
+++ 20201117 - KursDO/GrundlagenVererbung/GrundlagenVererbung/Sales.cs
@@ -7,12 +7,13 @@ using System.Threading.Tasks;
 namespace GrundlagenVererbung
 {
     class Sales : Emloyee //Ableitung - Die Klasse 'Sales' erbt jetzt die Klasse 'Emloyee'
+        //Sub-Klasse | Basis-KLasse
     {
         private Double _provision;
 
         //Konstruktor | Instanzieren
         public Sales(string vorname, string nachname, DateTime geburtsDatum, decimal gehalt,double _provision) 
-            : base(vorname, nachname, geburtsDatum, gehalt)
+            : base(vorname, nachname, geburtsDatum, gehalt) 
         {
             _provision = Provision;
         }
