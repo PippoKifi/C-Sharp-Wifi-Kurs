3028b649847cf8d475369f2c85be31509090be08
PippoKifi@Home <kipippo@posteo.at>
2020-11-19 21:46:38 +0100

    RandomAdv Add

--- 20201105 - KursOnline/Wifi.ToolLibrary_Attila/Wifi.ToolLibrary.TestApplication/Program.cs
+++ 20201105 - KursOnline/Wifi.ToolLibrary_Attila/Wifi.ToolLibrary.TestApplication/Program.cs
@@ -12,17 +12,37 @@ namespace Wifi.ToolLibrary.TestApplication
     {
         static void Main(string[] args)
         {
-            ConsoleTools.DisplayColoredMessage("Dies ist ein Test..", ConsoleColor.Red);
+            //ConsoleTools.DisplayColoredMessage("Dies ist ein Test..", ConsoleColor.Red);
 
-            double val1 = ConsoleTools.GetDouble("Bitte Wert eingeben: ");
+            //double val1 = ConsoleTools.GetDouble("Bitte Wert eingeben: ");
 
-            Console.WriteLine($"Der Wert war: {val1}");
+            //Console.WriteLine($"Der Wert war: {val1}");
 
-            ConsoleTools.DisplayColoredMessage("Test", ConsoleColor.Yellow);
+            //ConsoleTools.DisplayColoredMessage("Test", ConsoleColor.Yellow);
 
-            ConsoleTools.DisplayColoredMessage("Hallo");
+            //ConsoleTools.DisplayColoredMessage("Hallo");
 
-            Console.ReadLine();
+            //Console.ReadLine();
+
+            Console.WriteLine("Use STD Random");
+            Random rnd = new Random();
+            Console.WriteLine($" Zufällg zahl (1-10) {rnd.Next(1,10)}");
+            Console.WriteLine($" Zufällg zahl (1-10) {rnd.Next(1, 10)}");
+            Console.WriteLine($" Zufällg zahl (1-10) {rnd.Next(1, 10)}");
+            Console.WriteLine($" Zufällg zahl (1-10) {rnd.Next(1, 10)}");
+
+            Console.WriteLine("Use RandomAdv");
+            RandomAdv rndAdv = new RandomAdv();
+            Console.WriteLine($" Zufällg zahl (1-10) {rndAdv.Next(1, 10)}");
+            Console.WriteLine($" Zufällg zahl (1-10) {rndAdv.Next(1, 10)}");
+            Console.WriteLine($" Zufällg zahl (1-10) {rndAdv.Next(1, 10)}");
+
+            Console.WriteLine("Use RandomAdv whit NextString ");
+            Console.WriteLine($" Zufällg zahl (1-10) {rndAdv.NextString(25)}");
+            Console.WriteLine($" Zufällg zahl (1-10) {rndAdv.NextString(50)}");
+            Console.WriteLine($" Zufällg zahl (1-10) {rndAdv.NextString(75)}");
+
         }
+
     }
 }
