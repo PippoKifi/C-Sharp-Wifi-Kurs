860b3fbf4695eacb25990b81fe2c27c84f1dc96f
PippoKifi@Home <kipippo@posteo.at>
2020-11-24 21:45:05 +0100

    InterfaceGL

---
+++ 20201124 - KursOnline/InterfacesGL/InterfacesGL/DummyRepository.cs
@@ -0,0 +1,26 @@
+﻿using AbstrakteKlassen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesGL
+{
+    class DummyRepository : IDataRepository
+    {
+        public string Name => throw new NotImplementedException();
+
+        public int MaxSize => throw new NotImplementedException();
+
+        public string Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Write(string data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ 20201124 - KursOnline/InterfacesGL/InterfacesGL/IDataRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstrakteKlassen
+{
+    public interface IDataRepository //Intrface fangen immer mit 'I' an
+    {
+        //Interface Member IMMER PUBLIC + ABSTRACT
+
+        //Interface hat immer nur Public elemente (deshalb muss es nicht definiert werden)
+        // Jedes Member ist automatisch abstract (deshalb muss es nicht spezifisch declariert werden!)
+        string Name { get; }
+        
+        int MaxSize { get; }
+
+        void Write(string data);
+
+        string Read();
+    }
+}
