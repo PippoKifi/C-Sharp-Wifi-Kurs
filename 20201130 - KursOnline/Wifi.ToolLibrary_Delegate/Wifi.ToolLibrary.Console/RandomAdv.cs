bfe02c2bcd9c6ac2750ed773338b394d15e0542c
PippoKifi@Home <kipippo@posteo.at>
2020-11-30 21:36:44 +0100

    Delegate Exampel (ConsoleTools.GetInt  evolution)

---
+++ 20201130 - KursOnline/Wifi.ToolLibrary_Delegate/Wifi.ToolLibrary.Console/RandomAdv.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wifi.ToolLibrary
+{
+    public class RandomAdv : Random
+    {
+
+        //'ctor'
+        public RandomAdv() 
+        {
+
+        }
+
+        public RandomAdv(int Seed) : base (Seed)
+        {
+
+        }
+
+        /// <summary>
+        /// Generiert einen zufälligen String
+        /// </summary>
+        /// <param name="lenght">Definiert die länge von dem Zufälligem String</param>
+        /// <returns></returns>
+        public string NextString(int lenght)
+        {
+            //char[] myCharacterList = new char[20];
+            //int zufallsIndex = this.Next(0, 66);
+            //string zufallsString = string.Empty;
+            //zufallsString += myCharacterList[zufallsIndex].ToString();
+            //return zufallsString;
+
+            string randomString = string.Empty;
+            int randomChar = 0;
+            for (int i = 0; i < lenght; i++)
+            {
+                int isUpperCase = this.Next(0, 2); // 0 = False | 1 = True
+                if (isUpperCase == 0)
+                {
+                    randomChar = this.Next('a', 'z' + 1);
+                }
+                else
+                {
+                    randomChar = this.Next('A', 'Z' + 1 );
+                }
+                randomString += (char)randomChar;
+            }
+            return randomString;
+        }
+
+    }
+}
---
+++ 20201130 - KursOnline/Wifi.ToolLibrary_Delegate/Wifi.ToolLibrary.TestApplication/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Wifi.ToolLibrary.ConsoleIo;
+
+namespace Wifi.ToolLibrary.TestApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ConsoleTools.DisplayColoredMessage("Dies ist ein Test..", ConsoleColor.Red);
+
+            //double val1 = ConsoleTools.GetDouble("Bitte Wert eingeben: ");
+
+            //Console.WriteLine($"Der Wert war: {val1}");
+
+            //ConsoleTools.DisplayColoredMessage("Test", ConsoleColor.Yellow);
+
+            //ConsoleTools.DisplayColoredMessage("Hallo");
+
+            //Console.ReadLine();
+
+            //Console.WriteLine("Use STD Random");
+            //Random rnd = new Random();
+            //Console.WriteLine($" Zufällg zahl (1-10) {rnd.Next(1,10)}");
+            //Console.WriteLine($" Zufällg zahl (1-10) {rnd.Next(1, 10)}");
+            //Console.WriteLine($" Zufällg zahl (1-10) {rnd.Next(1, 10)}");
+            //Console.WriteLine($" Zufällg zahl (1-10) {rnd.Next(1, 10)}");
+
+            //Console.WriteLine("Use RandomAdv");
+            //RandomAdv rndAdv = new RandomAdv();
+            //Console.WriteLine($" Zufällg zahl (1-10) {rndAdv.Next(1, 10)}");
+            //Console.WriteLine($" Zufällg zahl (1-10) {rndAdv.Next(1, 10)}");
+            //Console.WriteLine($" Zufällg zahl (1-10) {rndAdv.Next(1, 10)}");
+
+            //Console.WriteLine("Use RandomAdv whit NextString ");
+            //Console.WriteLine($" Zufällg zahl (1-10) {rndAdv.NextString(25)}");
+            //Console.WriteLine($" Zufällg zahl (1-10) {rndAdv.NextString(50)}");
+            //Console.WriteLine($" Zufällg zahl (1-10) {rndAdv.NextString(75)}");
+
+           var geburtsJahr =  ConsoleTools.GetInt("In was für eine Geburtsjahr ", DisplayErrorOnSameLine);
+
+        }
+
+        private static void DisplayInputError(string errorMessage)
+        {
+            //Wie soll der Fehle dargestellt werden?
+            ConsoleTools.DisplayColoredMessage(errorMessage, ConsoleColor.Red);
+        }
+
+        static  void DisplayErrorOnSameLine(string errorMessage)
+        {
+            var oldYPosition = Console.CursorTop;
+            Console.SetCursorPosition(0, Console.WindowHeight - 3);
+            ConsoleTools.DisplayColoredMessage(errorMessage, ConsoleColor.Red);
+            Console.ReadLine();
+
+            //Löschen der Fehlermeldung
+            Console.SetCursorPosition(0, Console.WindowHeight - 3);
+            Console.WriteLine(new string(' ', errorMessage.Length));
+            Console.SetCursorPosition(0, oldYPosition);
+        }
+
+
+    }
+}
