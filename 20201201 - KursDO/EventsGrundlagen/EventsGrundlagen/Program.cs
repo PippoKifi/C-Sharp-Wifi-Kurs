3984b2caa777f062b7c2a7e48716c413d08aafc3
user <user@WIN10DEV>
2020-12-01 21:29:56 +0100

    EventsGrundlagen

--- 20201201 - KursDO/EventsGrundlagen/EventsGrundlagen/Program.cs
+++ 20201201 - KursDO/EventsGrundlagen/EventsGrundlagen/Program.cs
@@ -13,14 +13,26 @@ namespace EventsGrundlagen
             var myCar = new Car("SuperCar VW", 220);
             //myCar
 
+            myCar.CarExploded += MyCar_CarExploded; //Mit '+' an dem Event ANMELDEN
+                                                    //Mit '-' von Event ABMELDEN
+
             do
             {
                 Console.WriteLine(myCar);
                 myCar.SpeedUp(20);
+                                
 
             } while (myCar.State != State.Exploded);
-                
 
+
+            //Vom Ereignis Ebmelden
+            //myCar.CarExploded -= MyCar_CarExploded; //Event ABMELDEN
+
+        }
+
+        private static void MyCar_CarExploded(int currentSpeed, int maxSpeed)
+        {
+            Console.WriteLine("Leider ist das Auto Explodiert!");
         }
     }
 }
